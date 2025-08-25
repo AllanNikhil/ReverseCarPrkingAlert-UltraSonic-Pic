034c592b5875dd8f2409876cc7ac6aa142b07286
AllanNikhil <allannikhil18@gmail.com>
2025-08-25 21:30:30 +0530

    header added

---
+++ ultra.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   ultra.h
+ * Author: Asus
+ *
+ * Created on 12 August, 2025, 10:55 AM
+ */
+
+#ifndef ULTRASONIC_H
+#define	ULTRASONIC_H
+void timer();
+void display(int);
+void command(char,char);
+void enable();
+void delay(unsigned int);
+#endif	/* ULTRASONIC_H */
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#ifdef	__cplusplus
+}
+#endif
